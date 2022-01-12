6722bf8b0a69ec436f0ddfce232f3dc2630c67e0
AllanSam <93815162+Allansam95@users.noreply.github.com>
2022-01-12 08:55:08 +0530

    error fixed

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ var btntranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector(".output")
 
-//var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function errorHandler(error) {
